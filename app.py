4f33f68ae11a8386c9617cb95e53a016b7b07caf
hyejj19 <89173923+hyejj19@users.noreply.github.com>
2021-10-06 20:35:37 +0900

    Updated API

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import re
+<<<<<<< Updated upstream
 from flask import Flask, render_template, request, url_for, redirect, session
 import pyrebase
 import json
 import datetime
 from xml.sax.saxutils import escape
+=======
+from flask import Flask, render_template, request, url_for, redirect, session, jsonify
+import pyrebase
+import json
+import datetime
+
+>>>>>>> Stashed changes
 
 with open(r"auth/firebase.json") as f:  # 파이어베이스 주소
     config = json.load(f)
@@ -20,53 +28,52 @@ app = Flask(__name__)
 app.secret_key = "asfasdfsdagdfhsa"  # 시크릿 키
 
 
-# api data 함수
-def api_data(sido):
-    global all_cat_id
-    global all_sido
-    global all_notice_day
-    global all_notice_num
-    global all_cat_img
-    global all_cat_feature
-    global all_cat_color
-    global all_cat_weigth
-    global all_cat_gender
-    global all_care_name
-    global all_care_tel
-    global all_care_addr
-    all_cat_id = []  # '유기번호'
-    all_sido = []  # '지역'
-    all_notice_day = []  # 공고일
-    all_notice_num = []  # 공고 번호
-    all_cat_img = []  # 고양이사진 주소
-    all_cat_feature = []  # 특징
-    all_cat_color = []  # 고양이 색
-    all_cat_weigth = []  # 몸무게
-    all_cat_gender = []  # 성별
-    all_care_name = []  # 보호소 명
-    all_care_tel = []  # 보호소 연락처
-    all_care_addr = []  # 보호소 주소
+# # api data 함수
+# def api_data(sido):
+#     global all_cat_id
+#     global all_sido
+#     global all_notice_day
+#     global all_notice_num
+#     global all_cat_img
+#     global all_cat_feature
+#     global all_cat_color
+#     global all_cat_weigth
+#     global all_cat_gender
+#     global all_care_name
+#     global all_care_tel
+#     global all_care_addr
+#     all_cat_id = []  # '유기번호'
+#     all_sido = []  # '지역'
+#     all_notice_day = []  # 공고일
+#     all_notice_num = []  # 공고 번호
+#     all_cat_img = []  # 고양이사진 주소
+#     all_cat_feature = []  # 특징
+#     all_cat_color = []  # 고양이 색
+#     all_cat_weigth = []  # 몸무게
+#     all_cat_gender = []  # 성별
+#     all_care_name = []  # 보호소 명
+#     all_care_tel = []  # 보호소 연락처
+#     all_care_addr = []  # 보호소 주소
 
-    sido_cats_info = db.child("고양이").child(sido).get().val()
-    # print(sido_cats_info)
+#     sido_cats_info = db.child("고양이").child(sido).get().val()
+#     # print(sido_cats_info)
 
-    for sido_c_id in sido_cats_info:  # seoul_c_id 변수는 서울 고양이 유기번호를 뜻함
-        sido_cat = db.child("고양이").child(sido).child(
-            sido_c_id).get().val()  # 한마리 개체
-        # print(sido_c_id)
+#     for sido_c_id in sido_cats_info:  # sido_c_id 변수는 sido에 해당하는 모든 고양이 유기번호를 뜻함
+#         sido_cat = db.child("고양이").child(sido).child(sido_c_id).get().val()  # 한마리 개체
+#         # print(sido_c_id)
 
-        all_cat_id.append(sido_cat['유기번호'])
-        all_sido.append(sido_cat['지역'])
-        all_notice_day.append(sido_cat['공고일'])
-        all_notice_num.append(sido_cat['공고 번호'])
-        all_cat_img.append(sido_cat['고양이사진 주소'])
-        all_cat_feature.append(sido_cat['특징'])
-        all_cat_color.append(sido_cat['고양이 색'])
-        all_cat_weigth.append(sido_cat['몸무게'])
-        all_cat_gender.append(sido_cat['성별'])
-        all_care_name.append(sido_cat['보호소 명'])
-        all_care_tel.append(sido_cat['보호소 연락처'])
-        all_care_addr.append(sido_cat['보호소 주소'])
+#         all_cat_id.append(sido_cat['유기번호'])
+#         all_sido.append(sido_cat['지역'])
+#         all_notice_day.append(sido_cat['공고일'])
+#         all_notice_num.append(sido_cat['공고 번호'])
+#         all_cat_img.append(sido_cat['고양이사진 주소'])
+#         all_cat_feature.append(sido_cat['특징'])
+#         all_cat_color.append(sido_cat['고양이 색'])
+#         all_cat_weigth.append(sido_cat['몸무게'])
+#         all_cat_gender.append(sido_cat['성별'])
+#         all_care_name.append(sido_cat['보호소 명'])
+#         all_care_tel.append(sido_cat['보호소 연락처'])
+#         all_care_addr.append(sido_cat['보호소 주소'])
 
 
 # 대문 페이지
@@ -125,9 +132,8 @@ def login_done():
 def id_duplic():
     pass
 
-    # 로그아웃
 
-
+# 로그아웃
 @app.route("/log_out", methods=['get'])
 def login_out():
     session.pop("uid")  # 세션에 아이디 정보를 삭제
@@ -169,9 +175,10 @@ def signup_done():
                 return redirect(url_for("index"))  # 회원 가입 완료했으니 index 페이지로 가
 
 
-#  api 정보
+#  api 시도 받는 역할
 @app.route('/cat_card_sido', methods=['GET', 'POST'])
 def cat_card_sido():
+<<<<<<< Updated upstream
     api_data('경기도')
     return redirect(
         url_for('/cat_list.html'),
@@ -188,11 +195,25 @@ def cat_card_sido():
         all_care_tel=all_care_tel,  # 보호소 연락처
         all_care_addr=all_care_addr  # 보호소 주소
     )
+=======
+    global sido
+    sido = request.args.get('sido_give')
+    return sido
+>>>>>>> Stashed changes
+
+# api 정보 보내주는 역할
+@app.route('/cat_list_info', methods=['GET', 'POST'])
+def cat_list_info():
+    sido_cats_info = db.child("고양이").child(sido).child('유기번호').get().val()
+    return jsonify({'msg':sido_cats_info})
 
 
+# 카드 리스트 페이지 
 @app.route('/cat_list', methods=['GET', 'POST'])
 def cat_list():
     return render_template('/cat_list.html')
+
+
 
 
 if __name__ == "__main__":
